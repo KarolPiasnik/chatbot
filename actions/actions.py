ef2fef0e582c1bb3b2c99f8da4f51b3cbfedf6b1
“KarolPiasnik” <piasnik.karol@outlook.com>
2022-02-22 23:09:07 +0100

    basic working version in shell

--- actions/actions.py
+++ actions/actions.py
@@ -7,10 +7,11 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
+from typing import Any, Text, Dict, List
+import json
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 #
 #
 # class ActionHelloWorld(Action):
@@ -25,3 +26,43 @@
 #         dispatcher.utter_message(text="Hello World!")
 #
 #         return []
+class action_menu(Action):
+
+    def name(self) -> Text:
+        return "action_menu"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        f = open("data/menu.json")
+
+        data = json.load(f)
+        result = ""
+        result += "Menu is as below:\n"
+
+        for i in data["items"]:
+            result += i["name"] + ":\n" + "\tprice: " + str(i["price"]) + "PLN\n\tpreparation time: " + str(i["preparation_time"]*60) + " minutes\n"
+        dispatcher.utter_message(text=result)
+
+        return []
+
+class ActionOpeningHours(Action):
+
+    def name(self) -> Text:
+        return "action_opening_hours"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        f = open("data/opening_hours.json")
+
+        data = json.load(f)
+        result = ""
+        result += "Opening hours are as below:\n"
+
+        for i in data["items"]:
+            result += i + ": " + str(data["items"][i]["open"]) + ":00 - " + str(data["items"][i]["open"])+":00\n"
+        dispatcher.utter_message(text=result)
+
+        return []
