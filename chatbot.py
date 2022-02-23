638199432cd8da080a8f41da4f2d019c7a7be606
KarolPiasnik <piasnik.karol@outlook.com>
2022-02-23 21:47:03 +0100

    Delete not needed code

--- chatbot.py
+++ chatbot.py
@@ -3,22 +3,6 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
 
-def pretty_print_POST(req):
-    """
-    At this point it is completely built and ready
-    to be fired; it is "prepared".
-
-    However pay attention at the formatting used in 
-    this function because it is programmed to be pretty 
-    printed and may differ from the actual request.
-    """
-    print('{}\n{}\r\n{}\r\n\r\n{}'.format(
-        '-----------START-----------',
-        req.method + ' ' + req.url,
-        '\r\n'.join('{}: {}'.format(k, v) for k, v in req.headers.items()),
-        req.body,
-    ))
-
 @client.event
 async def on_ready():
     print('bot is ready')
